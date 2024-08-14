37e5d70c5bbe61bc594404285e6caf3da6fb04b2
RAHAL Rayane <91078977+ReyDioGames@users.noreply.github.com>
2024-08-14 15:20:30 +0100

    avancement sur le site

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navLinks = document.querySelectorAll('nav ul li a');
+    const sections = document.querySelectorAll('section');
+
+    function changeLinkState() {
+        let index = sections.length;
+
+        while (--index && window.scrollY + 50 < sections[index].offsetTop) { }
+
+        navLinks.forEach(link => link.classList.remove('active'));
+        navLinks[index].classList.add('active');
+    }
 
     navLinks.forEach(link => {
         link.addEventListener('click', (event) => {
@@ -10,4 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     });
+
+    changeLinkState();
+    window.addEventListener('scroll', changeLinkState);
 });
