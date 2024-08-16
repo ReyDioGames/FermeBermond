d982e87147921d45d7b072f6fb3d8dd0cb83aff3
RAHAL Rayane <91078977+ReyDioGames@users.noreply.github.com>
2024-08-16 14:58:00 +0100

    burger

--- scripts.js
+++ scripts.js
@@ -43,3 +43,25 @@
         });
     });
 })();
+// Sélectionner les éléments nécessaires
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+// Ajouter un événement au clic sur le burger
+burger.addEventListener('click', () => {
+    // Activer/Désactiver le menu
+    nav.classList.toggle('nav-active');
+
+    // Animation des liens
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+
+    // Animation du burger
+    burger.classList.toggle('toggle');
+});
